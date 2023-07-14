e2aefdf0f1efafb2ff87629305306f28675ec8cb
ARYAZAYO <79688242+ARYAZAYO@users.noreply.github.com>
2023-07-14 22:40:10 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(void) {
-  char ch = 'a';
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-  for (int i = 0; i < 26; i++) {
-    putchar(ch);
-    ch++;
-  }
+int main(void)
+{
+	char low;
 
-  putchar('\n');
-  ch = 'A';
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
 
-  for (int i = 0; i < 26; i++) {
-    putchar(ch);
-    ch++;
-  }
-
-  return 0;
+	return (0);
 }
-
