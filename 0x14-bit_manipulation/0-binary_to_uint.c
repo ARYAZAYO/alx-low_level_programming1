9c4387ecf3c5dfb2fd8eedea2f913f94a72a5d59
ARYAZAYO <aryazayokeith@gmail.com>
2023-08-31 08:00:42 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,21 +6,20 @@
  * By Kyth
  * Return: the converted number
  */
-unsigned int binary_to_uint(const char *b) {
-  unsigned int n = 0;
-  int i;
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int dec_val = 0;
 
-  if (b == NULL) {
-    return 0;
-  }
+	if (!b)
+		return (0);
 
-  for (i = strlen(b) - 1; i >= 0; i--) {
-    if (b[i] != '0' && b[i] != '1') {
-      return 0;
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    n += (b[i] - '0') * pow(2, strlen(b) - 1 - i);
-  }
-
-  return n;
+	return (dec_val);
 }
